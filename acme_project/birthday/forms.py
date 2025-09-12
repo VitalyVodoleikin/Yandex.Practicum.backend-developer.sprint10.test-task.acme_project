af2b74b8d4a2987ff3150958a3ad8f52e7d63e93
Vitaly Vodoleikin <vodoleikin@yandex.ru>
2025-09-12 20:35:54 +0300

    v0 12.09.2025.
    Added already files by sprint 10 "acme_project"

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,71 @@
+# ----------
+# birthday/forms.py
+from django import forms
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
+# Импортируем класс модели Birthday.
+from .models import Birthday
+
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+
+# Для использования формы с моделями меняем класс на forms.ModelForm.
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        date_field = forms.DateField(
+            widget=forms.DateInput(format='%Y-%m-%d'),
+            input_formats=['%Y-%m-%d']
+        )
+
+        # widgets = {
+        #     'birthday': forms.DateInput(attrs={'type': 'date'})
+        # }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0] 
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+
+
+
+
+
+
+# # ----------
+# # birthday/forms.py
+# from django import forms
+# 
+# 
+# class BirthdayForm(forms.Form):
+#     first_name = forms.CharField(label='Имя', max_length=20)
+#     last_name = forms.CharField(
+#         label='Фамилия', required=False, help_text='Необязательное поле'
+#     )
+#     birthday = forms.DateField(
+#         label='Дата рождения',
+#         # Указываем, что виджет для ввода даты должен быть с типом date.
+#         widget=forms.DateInput(attrs={'type': 'date'})
+#     )
