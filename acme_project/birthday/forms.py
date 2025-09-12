ba7608040449ba0944c312099acac0d3dd6f7229
vodoleikin <vodoleikin@yandex.ru>
2025-09-12 22:37:07 +0300

    v1 12.09.2025. lesson 10-1-2. Users_in Django, E-mails in Django.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -5,6 +5,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 # Импортируем класс модели Birthday.
 from .models import Birthday
+# Импорт функции для отправки почты.
+from django.core.mail import send_mail
 
 
 # Множество с именами участников Ливерпульской четвёрки.
@@ -37,17 +39,36 @@ class BirthdayForm(forms.ModelForm):
         # и возвращаем только первое имя.
         return first_name.split()[0] 
 
+    # def clean(self):
+    #     # Вызов родительского метода clean.
+    #     super().clean()
+    #     # Получаем имя и фамилию из очищенных полей формы.
+    #     first_name = self.cleaned_data['first_name']
+    #     last_name = self.cleaned_data['last_name']
+    #     # Проверяем вхождение сочетания имени и фамилии во множество имён.
+    #     if f'{first_name} {last_name}' in BEATLES:
+    #         raise ValidationError(
+    #             'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+    #         )
+        
+
     def clean(self):
-        # Вызов родительского метода clean.
         super().clean()
-        # Получаем имя и фамилию из очищенных полей формы.
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
-        # Проверяем вхождение сочетания имени и фамилии во множество имён.
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется 
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            ) 
+            )
 
 
 
